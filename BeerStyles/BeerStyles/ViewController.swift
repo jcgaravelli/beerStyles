6ccfb857318e09cec971b3801fb9f2770ea87a39
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-07-06 12:13:06 -0300

    hue

---
+++ BeerStyles/BeerStyles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerStyles
+//
+//  Created by Fernando H M Bastos on 7/6/15.
+//  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
