5f2974605567ce77ed36a06c8f4bc6f697a9dd57
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2015-07-08 11:07:09 -0300

    cagada

---
+++ BeerStyles/BeerStyles/DatabaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseManager.swift
+//  BeerStyles
+//
+//  Created by Paulo César Morandi Massuci on 07/07/15.
+//  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
