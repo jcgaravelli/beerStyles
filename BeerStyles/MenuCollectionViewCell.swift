5d9db9a9da11c18349a5c5c602193b18ff25843f
Matheus Santos Lopes <matheus.fcc.faj@gmail.com>
2015-07-11 01:44:19 -0300

    Tela inicial sem efeitos chamando uma nova view

---
+++ BeerStyles/MenuCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCollectionViewCell.swift
+//  BeerStyles
+//
+//  Created by Matheus Santos Lopes on 10/07/15.
+//  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
