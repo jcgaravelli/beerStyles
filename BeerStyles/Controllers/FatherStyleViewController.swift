71d9c99b84d4e7dcb1d834603bf6034f6c35e88f
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2015-07-14 11:31:58 -0300

    PFTableView

---
+++ BeerStyles/Controllers/FatherStyleViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FatherStyleViewController.swift
+//  BeerStyles
+//
+//  Created by Paulo César Morandi Massuci on 14/07/15.
+//  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import ParseUI
+import Parse
+
+
+class FatherStyleViewController: PFQueryTableViewController {
+    // Initialise the PFQueryTable tableview
+    override init(style: UITableViewStyle, className: String!) {
+        super.init(style: style, className: className)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Configure the PFQueryTableView
+        self.parseClassName = "FatherStyle"
+        self.textKey = "name"
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false
+    }
+    
+    // Define the query that will provide the data for the table view
+    override func queryForTable() -> PFQuery {
+        var query = PFQuery(className: "FatherStyle")
+        query.orderByAscending("name")
+        return query
+    }
+    
+    
+    
+}
+    
+    
+
+
