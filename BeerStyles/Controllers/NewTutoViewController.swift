db83382a4656dbe40ab534136bbf05539f74a891
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2015-07-23 15:31:40 -0300

    mudanças

---
+++ BeerStyles/Controllers/NewTutoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewTutoViewController.swift
+//  BeerStyles
+//
+//  Created by Paulo César Morandi Massuci on 23/07/15.
+//
+//
+
+import UIKit
+
+class NewTutoViewController: UIViewController {
+//    @IBOutlet weak var imageView: UIImageView
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        scrollView.contentSize = imageView.frame.size
+    
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
